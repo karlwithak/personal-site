826ff7682b2931d02ade7f34f26a3024cf53750e
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-05-10 11:40:10 -0400

    Vast improvements to home page and cloud animations. Now works properly on mobile.

--- js/clouds.js
+++ js/clouds.js
@@ -2,60 +2,61 @@
 
 $(function(){
 	var container = $(".container");
-	container.css("background", "skyblue");
-	var HEIGHT = $(window).height() - 20;
-	var WIDTH = $(window).width() - 20;
-	container.height(HEIGHT);
+	var HEIGHT = $(window).height();
+	var WIDTH = $(window).width();
 
-	function displayCloud() {
-	    this.elem = $(this.display).appendTo(container).css({
-	        'top': this.y + 'px',
-	        'opacity': this.opacity
-	    });
-	    this.moveCloud();
-	}
+    function Cloud() {
+        this.reset(true);
+    }
 
-	function Cloud() {
-	    this.display = '<img src="../img/cloud.png"/>';
-	    this.elem = null;
-	    this.y = 50 + (Math.random() * HEIGHT - 100);
-	    this.right = (Math.random() < 0.5 ? true : false);
-	    this.x = (this.right ? -111 : WIDTH + 111);
-	    this.opacity = 0.15 + (Math.random() * 0.5);
-	    this.speed = (0.5 + Math.random() * 1.9) * (this.right ? 1 : -1);
-	    this.displayCloud = displayCloud;
-	    this.moveCloud = moveCloud;
-	    this.displayCloud();
-	    $(this.elem).mouseover(function() {
-	        var that = this;
-	        $(this).fadeOut(1000).delay(1000);
-	        setTimeout(function(){this.elem.remove();}, 1000);
-	        cloudMaker();
-		});
-	}
-	
-	function cloudMaker() {
-	    new Cloud();
-	}
-	
-	function moveCloud() {
+    Cloud.prototype.display = '<img src="/img/cloud.png"/>';
+
+	Cloud.prototype.displayCloud = function() {
+        if (!this.elem) {
+            this.elem = $(this.display).appendTo(container);
+            var _this = this;
+            $(this.elem).mouseover(function() {
+                $(this).fadeOut(1000, function() {
+                    clearTimeout(_this.timeout);
+                    _this.reset(false);
+                });
+            })
+        }
+        this.elem.css({
+            'top': this.y + 'px',
+            'opacity': this.opacity
+        });
+        $(this.elem).show();
+	};
+
+    Cloud.prototype.reset = function(firstTime) {
+        this.right = Math.random() < 0.5;
+        if (firstTime) {
+            this.x = Math.random() * WIDTH;
+        } else {
+            this.x = (this.right ? -110 : WIDTH + 110);
+        }
+        this.y = Math.random() * (HEIGHT - 100);
+        this.opacity = 0.15 + (Math.random() * 0.3);
+        this.speed = (0.5 + Math.random()) * (this.right ? 1 : -1);
+        this.displayCloud();
+        this.moveCloud();
+    };
+
+	Cloud.prototype.moveCloud = function() {
 	    this.elem.css("left", this.x + "px");
 	    this.x += this.speed;
-	    var that = this;
 	    if ((this.right && this.x > WIDTH + 55) || (!this.right && this.x < -111)) {
-	        this.elem.css("display", "none");
-	        this.elem.remove();
-	        return;
-	    } else {
-	        setTimeout(function () {
-	            that.moveCloud();
-	        }, 40);
-	    }
-	}
-	
-	cloudMaker();
-	cloudMaker();
-	setInterval(function(){
-	    cloudMaker();
-	}, (WIDTH * 15) + Math.random() * 5000);
+            clearTimeout(this.timeout);
+            this.reset(false);
+        }
+        var _this = this;
+        this.timeout = setTimeout(function () {
+            _this.moveCloud();
+        }, 40);
+    };
+
+    for (var i = 0; i < 6; i++) {
+        new Cloud();
+    }
 });
